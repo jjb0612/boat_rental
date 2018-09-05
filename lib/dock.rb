48fc8e16f9bd85aa25cbbe24540662f7c909e1f7
Joshua Baik <jjbaik123@gmail.com>
2018-09-05 14:40:59 -0600

    made revenue method with test and changed log_hour method

--- lib/dock.rb
+++ lib/dock.rb
@@ -21,9 +21,20 @@ class Dock
     end
 
     def log_hour
+        time_limit = @max_rental_time
         @boats.each do |boat|
-            boat.add_hour
+            if boat.hours_rented < time_limit
+                boat.add_hour
+            end
         end
+    end
+
+    def revenue
+        total = 0
+        @boats.each do |boat|
+            total += (boat.hours_rented * boat.price_per_hour)
+        end
+        total
     end
 
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -31,7 +31,7 @@ class DockTest < Minitest::Test
     end
 
     def test_log_hour_will_add_one_to_hours_rented
-        dock = Dock.new("The Rowing Dock", 3)
+       dock = Dock.new("The Rowing Dock", 3)
        kayak_1 = Boat.new(:kayak, 20)
        kayak_2 = Boat.new(:kayak, 20)    
        canoe = Boat.new(:canoe, 25)   
@@ -45,6 +45,45 @@ class DockTest < Minitest::Test
        dock.log_hour
        assert_equal 2, kayak_1.hours_rented
        assert_equal 2, kayak_2.hours_rented
+    end
+
+    def test_log_hour_will_not_add_over_the_max_rental_time
+        dock = Dock.new("The Rowing Dock", 3)
+       kayak_1 = Boat.new(:kayak, 20)
+       kayak_2 = Boat.new(:kayak, 20)    
+       canoe = Boat.new(:canoe, 25)   
+       sup_1 = Boat.new(:standup_paddle_board, 15)    
+       sup_2 = Boat.new(:standup_paddle_board, 15)    
+       patrick = Renter.new("Patrick Star", "4242424242424242")
+       eugene = Renter.new("Eugene Crabs", "1313131313131313")
+       dock.rent(kayak_1, patrick)
+       dock.rent(kayak_2, patrick)
+       dock.log_hour
+       dock.log_hour
+       dock.log_hour
+       dock.log_hour
+       assert_equal 3, kayak_1.hours_rented
+       assert_equal 3, kayak_2.hours_rented
+    end
+
+    def test_it_will_stop_accumulating_revenue_when_boat_is_returned
+        
+    end
+
+    def test_it_can_calculate_revenue
+        dock = Dock.new("The Rowing Dock", 3)
+        kayak_1 = Boat.new(:kayak, 20)
+        kayak_2 = Boat.new(:kayak, 20)    
+        canoe = Boat.new(:canoe, 25)   
+        sup_1 = Boat.new(:standup_paddle_board, 15)    
+        sup_2 = Boat.new(:standup_paddle_board, 15)    
+        patrick = Renter.new("Patrick Star", "4242424242424242")
+        eugene = Renter.new("Eugene Crabs", "1313131313131313")
+        dock.rent(kayak_1, patrick)
+        dock.rent(kayak_2, patrick)
+        dock.log_hour
+        dock.log_hour
+        assert_equal 80, dock.revenue
     end
 
 end
